51bae7c676cfd8ac04b9c329dfcdf0afe9dc3f18
Ryan Rolnicki <rrolnicki@indeed.com>
2021-01-10 08:46:53 +0900

    Color as context

--- frontend/web-client/src/Components/DrawingControls.js
+++ frontend/web-client/src/Components/DrawingControls.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import styled from 'styled-components'
 import { ChromePicker } from 'react-color'
+import * as Context from '../Context'
 
 const Container = styled.div`
 
@@ -8,18 +9,59 @@ const Container = styled.div`
 
 const ControlPanel = styled.div`
     background-color: #151515aa;
+    color: #e6e6e6;
     width: 25vh;
     padding: 20px 20px;
     margin: 30px 50px;
 `
 
+const SVG = styled.svg`
+    display: inline-flex;
+    width: 30px;
+    height: 30px;
+    margin: 0px 5px;
+`
+
+function ColorCircle(props) {
+    return (
+        <SVG>
+            <circle cx="15" cy="15" r="15" stroke={props.color} stroke-width="1" fill={props.color} />
+        </SVG>
+    )
+}
+
+const ColorCircleContainer = styled.div`
+    display: flex;
+    justify-content: left;
+    align-items: center;
+`
+const ColorPaletteContainer = styled.div`
+    margin-top: 8px;
+`
+
+function ColorPalette() {
+    const [color, setColor] = React.useContext(Context.ActiveColorContext)
+    console.log(color)
+
+    return (
+        <ColorPaletteContainer>
+            <div>Palette</div>
+            <ColorCircleContainer>
+                <ColorCircle color={color.hex} />
+                <ColorCircle color="green" />
+            </ColorCircleContainer>
+        </ColorPaletteContainer>
+    )
+}
+
 export default function DrawingControls() {
-    const [color, setColor] = React.useState("orange")
+    const [color, setColor] = React.useContext(Context.ActiveColorContext)
+    console.log('Color: ' + color)
     return (
         <Container>
             <ControlPanel>
                 <ChromePicker disableAlpha={true} color={color} onChange={h => setColor(h)} />
-
+                <ColorPalette />
             </ControlPanel>
         </Container>
     )
--- frontend/web-client/src/Context.js
+++ frontend/web-client/src/Context.js
@@ -1,14 +1,19 @@
 import React from 'react';
-export const UserContext = React.createContext();
-export const ActivePageContext = React.createContext();
+export const UserContext = React.createContext()
+export const ActivePageContext = React.createContext()
+export const ActiveColorContext = React.createContext("red")
 
 export function AppContextProvider(props) {
     const [user, setUser] = React.useState(null)
     const [activePage, setActivePage] = React.useState(null)
+    const [activeColor, setActiveColor] = React.useState("red")
+
     return (
         <UserContext.Provider value={[user, setUser]}>
             <ActivePageContext.Provider value={[activePage, setActivePage]}>
-                {props.children}
+                <ActiveColorContext.Provider value={[activeColor, setActiveColor]}>
+                    {props.children}
+                </ActiveColorContext.Provider>
             </ActivePageContext.Provider>
         </UserContext.Provider>
     )
